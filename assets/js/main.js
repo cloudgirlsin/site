1a9f1ef75d27736e28f006daac5f800bf2f1afc5
Edlaine Pontes <edlaine.psico@gmail.com>
2020-08-12 13:08:11 -0300

    correção footer e navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,14 +17,7 @@
   });
 
   // Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
-    } else {
-      $('.back-to-top').fadeOut('slow');
-    }
-  });
-  $('.back-to-top').click(function() {
+   $('.back-to-top').click(function() {
     $('html, body').animate({
       scrollTop: 0
     }, 1500, 'easeInOutExpo');
